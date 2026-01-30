315d949d091ff3ec655c58ad4afbbb8c10caa8a1
Tomasposada26 <tomasposada26@gmail.com>
2026-01-30 14:36:35 -0500

    Add input validation middleware for tasks and update checklist

--- task-manager-fullstack/backend/src/routes/taskRoutes.ts
+++ task-manager-fullstack/backend/src/routes/taskRoutes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 import { createTask, getTasks, getTaskById, updateTask, deleteTask } from '../controllers/taskController';
 import { authMiddleware } from '../middlewares/authMiddleware';
+import { validateTaskInput } from '../middlewares/validateTaskInput';
 
 const router = Router();
 
 router.use(authMiddleware);
 
-router.post('/', createTask);
+
+router.post('/', validateTaskInput, createTask);
 router.get('/', getTasks);
 router.get('/:id', getTaskById);
-router.put('/:id', updateTask);
+router.put('/:id', validateTaskInput, updateTask);
 router.delete('/:id', deleteTask);
 
 export default router;
